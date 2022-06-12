adcd4a88a54ad40360a28bdf9051af883002e862
M L <magiconion_M@hotmail.com>
2022-06-12 12:30:15 +0100

    fix scripts

---
+++ Azure-Pre-Reqs/Prepare-RBAC-ACR.ps1
@@ -0,0 +1,14 @@
+#Log into Azure
+#az login
+
+# Setup Variables. (provide your ACR name)
+$appName="GitHub-ACI-Deploy"
+$acrName="<ACRName>"
+$region = "uksouth"
+
+# Create AAD App and Service Principal and assign to RBAC Role to push and pull images from ACR
+$acrId = az acr show --name "$acrName" --query id --output tsv
+az ad sp create-for-rbac --name $appName `
+    --role "Contributor" `
+    --scopes "$acrId" `
+    --sdk-auth
